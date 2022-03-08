bbfa3520eda52bcf0004d088bd48652039cc5e92
SirToffski <47118864+SirToffski@users.noreply.github.com>
2022-03-08 15:59:42 -0400

    Update mesa_git_aur_ccm.sh

--- mesa_git_aur_ccm.sh
+++ mesa_git_aur_ccm.sh
@@ -1,34 +1,87 @@
 #!/usr/bin/env bash
 
-script_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+elevated_build_tasks() {
 
-clone_upstream() {
-    mkdir -p /home/toffski/aur-pkg/mesa-git-aur
-    cd /home/toffski/aur-pkg/mesa-git-aur || exit
+    build_python-sphinx-automodapi() {
+        pacman -Syyu --noconfirm
+        ccm n
+        sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/usr\/share\/devtools\/makepkg-x86_64\.conf"/g' \
+            /home/toffski/.config/clean-chroot-manager.conf
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/python-sphinx-automodapi || exit
+        ccm s
+    }
 
-    rm -rf ./python-sphinx-automodapi
-    rm -rf ./llvm-git
-    rm -rf ./lib32-llvm-git
-    rm -rf ./mesa-git
-    rm -rf ./lib32-mesa-git
-    rm -rf ./xf86-video-amdgpu-git
-    # rm -rf ./xf86-video-intel-git
+    build_llvm-git() {
+        pacman -Syyu --noconfirm
+        ccm n
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/llvm-git || exit
+        ccm s
+    }
 
-    git clone https://aur.archlinux.org/python-sphinx-automodapi.git
-    git clone https://aur.archlinux.org/llvm-git.git
-    git clone https://aur.archlinux.org/lib32-llvm-git.git
-    git clone https://aur.archlinux.org/mesa-git.git
-    git clone https://aur.archlinux.org/lib32-mesa-git.git
-    git clone https://aur.archlinux.org/xf86-video-amdgpu-git.git
-    # mkdir -p /home/toffski/aur-pkg/mesa-git-aur/xf86-video-intel-git
-    
-    sudo ccm d
-    repo-add /home/toffski/toff_build_repo/toff-repo.db.tar.gz
+    build_mesa-git() {
+        pacman -Syyu --noconfirm
+        ccm n
+        sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/toffski\/\.makepkg\.laptop\.o3\.conf\"/g' \
+            /home/toffski/.config/clean-chroot-manager.conf
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/mesa-git || exit
+        sed -i 's/ MESA_WHICH_LLVM=4/ MESA_WHICH_LLVM=2/g' PKGBUILD
+        sed -i 's/gallium-opencl=icd/gallium-opencl=disabled/g' PKGBUILD
+        ccm s
+    }
+
+    build_lib32-llvm-git() {
+        pacman -Syyu --noconfirm
+        ccm n
+        sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/usr\/share\/devtools\/makepkg-x86_64\.conf"/g' \
+            /home/toffski/.config/clean-chroot-manager.conf
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/lib32-llvm-git || exit
+        sed -i 's/ MESA_WHICH_LLVM=4/ MESA_WHICH_LLVM=2/g' PKGBUILD
+        ccm s
+    }
+
+    build_lib32-mesa-git() {
+        pacman -Syyu --noconfirm
+        ccm n
+        sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/toffski\/makepkg-x86_64_O3\.conf\"/g' \
+            /home/toffski/.config/clean-chroot-manager.conf
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/lib32-mesa-git || exit
+        sed -i 's/ MESA_WHICH_LLVM=4/ MESA_WHICH_LLVM=2/g' PKGBUILD
+        sed -i "s/'lib32-libvdpau'//g" PKGBUILD
+        sed -i "s/'lib32-zstd')/'lib32-zstd' 'lib32-libvdpau')/g" PKGBUILD
+        ccm s
+    }
+
+    build_xf86-video-amdgpu-git() {
+        pacman -Syyu --noconfirm
+        ccm n
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/xf86-video-amdgpu-git || exit
+        ccm s
+    }
+
+    # build_xf86-video-intel-git() {
+    #     pacman -Syyu --noconfirm
+    #     ccm n
+    #     ccm c
+    #     cd /home/toffski/aur-pkg/mesa-git-aur/xf86-video-intel-git || exit
+    #     wget 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/xf86-video-intel/trunk/PKGBUILD'
+    #     wget 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/xf86-video-intel/trunk/xf86-video-intel.install'
+    #     ccm s
+    # }
+
+    build_python-sphinx-automodapi || exit
+    build_llvm-git || exit
+    build_mesa-git || exit
+    build_lib32-llvm-git || exit
+    build_lib32-mesa-git || exit
+    build_xf86-video-amdgpu-git || exit
+    # build_xf86-video-intel-git || exit
+
 }
 
-main() {
-    clone_upstream
-    sudo bash "$script_directory/mesa_git_aur_ccm_build.sh"
-}
-
-main
+elevated_build_tasks
