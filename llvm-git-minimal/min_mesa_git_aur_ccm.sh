86a8356ebb50656d842f2c9fe3f1723b4eb27650
SirToffski <47118864+SirToffski@users.noreply.github.com>
2022-03-09 22:09:17 +0000

    Add llvm-min scripts

---
+++ llvm-git-minimal/min_mesa_git_aur_ccm.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+script_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+clone_upstream() {
+    mkdir -p /home/toffski/aur-pkg/mesa-git-aur
+    cd /home/toffski/aur-pkg/mesa-git-aur || exit
+
+    rm -rf ./python-sphinx-automodapi
+    rm -rf ./llvm-minimal-git
+    rm -rf ./lib32-llvm-minimal-git
+    rm -rf ./mesa-git
+    rm -rf ./lib32-mesa-git
+    rm -rf ./xf86-video-amdgpu-git
+    # rm -rf ./xf86-video-intel-git
+
+    git clone https://aur.archlinux.org/python-sphinx-automodapi.git
+    git clone https://aur.archlinux.org/llvm-minimal-git.git
+    git clone https://aur.archlinux.org/lib32-llvm-minimal-git.git
+    git clone https://aur.archlinux.org/mesa-git.git
+    git clone https://aur.archlinux.org/lib32-mesa-git.git
+    git clone https://aur.archlinux.org/xf86-video-amdgpu-git.git
+    # mkdir -p /home/toffski/aur-pkg/mesa-git-aur/xf86-video-intel-git
+    
+    sudo ccm d
+    repo-add /home/toffski/toff_build_repo/toff-repo.db.tar.gz
+}
+
+main() {
+    clone_upstream
+    sudo bash "$script_directory/min_mesa_git_aur_ccm_build.sh"
+}
+
+main
---
+++ llvm-git-minimal/min_mesa_git_aur_ccm_build.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+elevated_build_tasks() {
+
+    build_python-sphinx-automodapi() {
+        pacman -Syyu --noconfirm
+        ccm n
+        sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/usr\/share\/devtools\/makepkg-x86_64\.conf"/g' \
+            /home/toffski/.config/clean-chroot-manager.conf
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/python-sphinx-automodapi || exit
+        ccm s
+    }
+
+    build_llvm-minimal-git() {
+        pacman -Syyu --noconfirm
+        ccm n
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/llvm-minimal-git || exit
+        ccm s
+    }
+
+    build_mesa-git() {
+        pacman -Syyu --noconfirm
+        ccm n
+        sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/toffski\/makepkg-x86_64_O3\.conf\"/g' \
+            /home/toffski/.config/clean-chroot-manager.conf
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/mesa-git || exit
+        sed -i 's/ MESA_WHICH_LLVM=4/ MESA_WHICH_LLVM=1/g' PKGBUILD
+        sed -i 's/gallium-opencl=icd/gallium-opencl=disabled/g' PKGBUILD
+        ccm s
+    }
+
+    build_lib32-llvm-minimal-git() {
+        pacman -Syyu --noconfirm
+        ccm n
+        sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/usr\/share\/devtools\/makepkg-x86_64\.conf"/g' \
+            /home/toffski/.config/clean-chroot-manager.conf
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/lib32-llvm-minimal-git || exit
+        ccm s
+    }
+
+    build_lib32-mesa-git() {
+        pacman -Syyu --noconfirm
+        ccm n
+        sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/toffski\/makepkg-x86_64_O3\.conf\"/g' \
+            /home/toffski/.config/clean-chroot-manager.conf
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/lib32-mesa-git || exit
+        sed -i 's/ MESA_WHICH_LLVM=4/ MESA_WHICH_LLVM=1/g' PKGBUILD
+        ccm s
+    }
+
+    build_xf86-video-amdgpu-git() {
+        pacman -Syyu --noconfirm
+        ccm n
+        ccm c
+        cd /home/toffski/aur-pkg/mesa-git-aur/xf86-video-amdgpu-git || exit
+        ccm s
+    }
+
+    # build_xf86-video-intel-git() {
+    #    pacman -Syyu --noconfirm
+    #    ccm n
+    #    ccm c
+    #    cd /home/toffski/aur-pkg/mesa-git-aur/xf86-video-intel-git || exit
+    #    wget 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/xf86-video-intel/trunk/PKGBUILD'
+    #    wget 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/xf86-video-intel/trunk/xf86-video-intel.install'
+    #    ccm s
+    # }
+
+    build_python-sphinx-automodapi || exit
+    build_llvm-minimal-git || exit
+    build_mesa-git || exit
+    build_lib32-llvm-minimal-git || exit
+    build_lib32-mesa-git || exit
+    build_xf86-video-amdgpu-git || exit
+    # build_xf86-video-intel-git || exit
+
+}
+
+elevated_build_tasks
