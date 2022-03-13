7578f9a41e3c3ae607cf7deaddcee87dd03d51d2
SirToffski <47118864+SirToffski@users.noreply.github.com>
2022-03-13 13:38:10 -0300

    Update min_mesa_git_aur_ccm_build.sh

--- llvm-git-minimal/min_mesa_git_aur_ccm_build.sh
+++ llvm-git-minimal/min_mesa_git_aur_ccm_build.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+clone_directory="/home/$USER/aur-pkg/mesa-git-aur"
+
 elevated_build_tasks() {
 
     build_python-sphinx-automodapi() {
         pacman -Syyu --noconfirm
         ccm n
+        # make sure python-sphinx-automodapi and llvm are compiled -march=x86-64 -mtune=generic because
+        # we are building on a remove machine
         sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/usr\/share\/devtools\/makepkg-x86_64\.conf"/g' \
-            /home/toffski/.config/clean-chroot-manager.conf
+            /home/"$USER"/.config/clean-chroot-manager.conf
         ccm c
-        cd /home/toffski/aur-pkg/mesa-git-aur/python-sphinx-automodapi || exit
+        cd "$clone_directory"/python-sphinx-automodapi || exit
         ccm s
     }
 
@@ -16,39 +20,48 @@ elevated_build_tasks() {
         pacman -Syyu --noconfirm
         ccm n
         ccm c
-        cd /home/toffski/aur-pkg/mesa-git-aur/llvm-minimal-git || exit
+        cd "$clone_directory"/llvm-minimal-git || exit
         ccm s
     }
 
     build_mesa-git() {
         pacman -Syyu --noconfirm
         ccm n
-        sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/toffski\/makepkg-x86_64_O3\.conf\"/g' \
-            /home/toffski/.config/clean-chroot-manager.conf
+        # build mesa-git with optimizations for ivybridge;
+        # to do that, we'll specify a different makepkg.conf file
+        sed -i -E "s/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/$USER\/makepkg-x86_64_O3\.conf\"/g" \
+            /home/"$USER"/.config/clean-chroot-manager.conf
         ccm c
-        cd /home/toffski/aur-pkg/mesa-git-aur/mesa-git || exit
+        cd "$clone_directory"/mesa-git || exit
+        # use aur llvm-minimal-git
         sed -i 's/ MESA_WHICH_LLVM=4/ MESA_WHICH_LLVM=1/g' PKGBUILD
-        sed -i 's/gallium-opencl=icd/gallium-opencl=disabled/g' PKGBUILD
+        # optionally, uncomment below to build without OpenCL
+        # sed -i 's/gallium-opencl=icd/gallium-opencl=disabled/g' PKGBUILD
         ccm s
     }
 
     build_lib32-llvm-minimal-git() {
         pacman -Syyu --noconfirm
         ccm n
+        # make sure python-sphinx-automodapi and llvm are compiled -march=x86-64 -mtune=generic because
+        # we are building on a remove machine
         sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/usr\/share\/devtools\/makepkg-x86_64\.conf"/g' \
-            /home/toffski/.config/clean-chroot-manager.conf
+            /home/"$USER"/.config/clean-chroot-manager.conf
         ccm c
-        cd /home/toffski/aur-pkg/mesa-git-aur/lib32-llvm-minimal-git || exit
+        cd "$clone_directory"/lib32-llvm-minimal-git || exit
         ccm s
     }
 
     build_lib32-mesa-git() {
         pacman -Syyu --noconfirm
         ccm n
-        sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/toffski\/makepkg-x86_64_O3\.conf\"/g' \
-            /home/toffski/.config/clean-chroot-manager.conf
+        # build mesa-git with optimizations for ivybridge;
+        # to do that, we'll specify a different makepkg.conf file
+        sed -i -E "s/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/$USER\/makepkg-x86_64_O3\.conf\"/g" \
+            /home/"$USER"/.config/clean-chroot-manager.conf
         ccm c
-        cd /home/toffski/aur-pkg/mesa-git-aur/lib32-mesa-git || exit
+        cd "$clone_directory"/lib32-mesa-git || exit
+        # use aur lib32-llvm-minimal-git
         sed -i 's/ MESA_WHICH_LLVM=4/ MESA_WHICH_LLVM=1/g' PKGBUILD
         ccm s
     }
@@ -57,7 +70,7 @@ elevated_build_tasks() {
         pacman -Syyu --noconfirm
         ccm n
         ccm c
-        cd /home/toffski/aur-pkg/mesa-git-aur/xf86-video-amdgpu-git || exit
+        cd "$clone_directory"/xf86-video-amdgpu-git || exit
         ccm s
     }
 
@@ -65,7 +78,7 @@ elevated_build_tasks() {
     #    pacman -Syyu --noconfirm
     #    ccm n
     #    ccm c
-    #    cd /home/toffski/aur-pkg/mesa-git-aur/xf86-video-intel-git || exit
+    #    cd "$clone_directory"/xf86-video-intel-git || exit
     #    wget 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/xf86-video-intel/trunk/PKGBUILD'
     #    wget 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/xf86-video-intel/trunk/xf86-video-intel.install'
     #    ccm s
