96725e0da9bc7c1fcafb629a5576fd00142db997
SirToffski <47118864+SirToffski@users.noreply.github.com>
2022-03-13 13:17:48 -0300

    Update min_mesa_git_aur_ccm.sh

--- llvm-git-minimal/min_mesa_git_aur_ccm.sh
+++ llvm-git-minimal/min_mesa_git_aur_ccm.sh
@@ -2,9 +2,27 @@
 
 script_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+clone_directory="/home/$USER/aur-pkg/mesa-git-aur"
+local_repo_directory="/home/$USER/${USER}_build_repo"
+
+date_dir="$(date +%d-%b-%Y)"
+
+backup_existing_pkg() {
+
+    if [[ -d "$local_repo_directory" ]]; then
+        cd "$local_repo_directory"
+        mkdir -p /home/"$USER"/pkg/"$date_dir"
+
+        for pkg in ./*.pkg.tar.zst; do
+            cp "$pkg" /home/"$USER"/pkg/"$date_dir"/"$pkg"
+        done
+
+    fi
+}
+
 clone_upstream() {
-    mkdir -p /home/toffski/aur-pkg/mesa-git-aur
-    cd /home/toffski/aur-pkg/mesa-git-aur || exit
+    mkdir -p "$clone_directory"
+    cd "$clone_directory" || exit
 
     rm -rf ./python-sphinx-automodapi
     rm -rf ./llvm-minimal-git
@@ -12,7 +30,6 @@ clone_upstream() {
     rm -rf ./mesa-git
     rm -rf ./lib32-mesa-git
     rm -rf ./xf86-video-amdgpu-git
-    # rm -rf ./xf86-video-intel-git
 
     git clone https://aur.archlinux.org/python-sphinx-automodapi.git
     git clone https://aur.archlinux.org/llvm-minimal-git.git
@@ -20,14 +37,19 @@ clone_upstream() {
     git clone https://aur.archlinux.org/mesa-git.git
     git clone https://aur.archlinux.org/lib32-mesa-git.git
     git clone https://aur.archlinux.org/xf86-video-amdgpu-git.git
-    # mkdir -p /home/toffski/aur-pkg/mesa-git-aur/xf86-video-intel-git
-    
+
+    #backup all existing packages in the build repo
+    backup_existing_pkg
+    # clear everything out from existing local repository
     sudo ccm d
-    repo-add /home/toffski/toff_build_repo/toff-repo.db.tar.gz
+    # re-create our local repo
+    repo-add "$local_repo_directory"/"${USER}"-repo.db.tar.gz
 }
 
 main() {
     clone_upstream
+    # Having a separate script for building multiple packages in CHROOT
+    # ensures sudo does not time out and ask for a password again
     sudo bash "$script_directory/min_mesa_git_aur_ccm_build.sh"
 }
 
