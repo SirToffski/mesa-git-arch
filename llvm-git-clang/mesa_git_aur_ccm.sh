7026156fbd71bac497c110c16f0f5764065bd2a4
SirToffski <47118864+SirToffski@users.noreply.github.com>
2022-06-05 11:52:41 -0300

    Update llvm build script

--- llvm-git-clang/mesa_git_aur_ccm.sh
+++ llvm-git-clang/mesa_git_aur_ccm.sh
@@ -8,29 +8,14 @@ clone_upstream() {
 
     rm -rf ./python-sphinx-automodapi
     rm -rf ./llvm-git
-    # rm -rf ./lib32-llvm-git
     rm -rf ./mesa-git
-    # rm -rf ./lib32-mesa-git
     rm -rf ./xf86-video-amdgpu-git
-    # rm -rf ./xf86-video-intel-git
 
     git clone https://aur.archlinux.org/python-sphinx-automodapi.git
     git clone https://github.com/SirToffski/llvm-git-PKGBUILD.git llvm-git
-    #git clone https://aur.archlinux.org/llvm-git.git
-    #git clone https://aur.archlinux.org/llvm-git.git llvm-git-O3
-    #git clone https://aur.archlinux.org/lib32-llvm-git.git
-    # git clone https://gist.github.com/SirToffski/37b5022a05ab8a62d9e26270912da7b7 mesa-git
-    #cd /home/toffski/aur-pkg/mesa-git-aur/mesa-git
-    #rm PKGBUILD
-    #wget --https-only 'https://gist.githubusercontent.com/SirToffski/37b5022a05ab8a62d9e26270912da7b7/raw/b470f5ac465da63f083cfd86ae75ed18d505dec1/clover-llvm.patch'
-    #wget --https-only 'https://gist.githubusercontent.com/SirToffski/37b5022a05ab8a62d9e26270912da7b7/raw/b470f5ac465da63f083cfd86ae75ed18d505dec1/PKGBUILD'
-    #cd /home/toffski/aur-pkg/mesa-git-aur || exit
-    #git clone https://aur.archlinux.org/mesa-git.git
     git clone https://github.com/SirToffski/mesa-git-PKGBUILD.git mesa-git
-    # git clone https://aur.archlinux.org/lib32-mesa-git.git
     git clone https://aur.archlinux.org/xf86-video-amdgpu-git.git
-    # mkdir -p /home/toffski/aur-pkg/mesa-git-aur/xf86-video-intel-git
-    
+
     sudo ccm d
     repo-add /home/toffski/toff_build_repo/toff-repo.db.tar.gz
 }
--- llvm-git-clang/mesa_git_aur_ccm_build.sh
+++ llvm-git-clang/mesa_git_aur_ccm_build.sh
@@ -5,12 +5,13 @@ elevated_build_tasks() {
     build_python-sphinx-automodapi() {
         pacman -Syyu --noconfirm
         ccm n
+
         sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/toffski\/makepkg-x86_64_LLVM\.conf\"/g' \
             /home/toffski/.config/clean-chroot-manager.conf
+
         sed -i -E 's/.*USELLVM.*/USELLVM=1/g' \
             /home/toffski/.config/clean-chroot-manager.conf
-        # sed -i -E 's/.*CUSTOM_PACMAN_CONF.*/#CUSTOM_PACMAN_CONF=\"\/etc\/pacman\.conf"/g' \
-        #     /home/toffski/.config/clean-chroot-manager.conf
+
         ccm c
         cd /home/toffski/arch-llvm/mesa-git-aur/python-sphinx-automodapi || exit
         ccm s
@@ -18,8 +19,6 @@ elevated_build_tasks() {
 
     build_llvm-git() {
         pacman -Syyu --noconfirm
-        # sed -i -E 's/.*CUSTOM_PACMAN_CONF.*/#CUSTOM_PACMAN_CONF=\"\/etc\/pacman\.conf"/g' \
-        #     /home/toffski/.config/clean-chroot-manager.conf
         ccm n
         ccm c
         cd /home/toffski/arch-llvm/mesa-git-aur/llvm-git || exit
@@ -30,61 +29,33 @@ elevated_build_tasks() {
         cd /home/toffski/llvm-git || exit
         rm -rf ./*
 
-        cp /home/toffski/toff_build_repo/llvm-git-*.pkg.tar.zst \
-            /home/toffski/llvm-git/llvm-git.pkg.tar.zst
+        cd /home/toffski/toff_build_repo || exit
 
-        cp /home/toffski/toff_build_repo/llvm-libs-git-*.pkg.tar.zst \
-            /home/toffski/llvm-git/llvm-libs-git.pkg.tar.zst
+        for file in llvm*.pkg.tar.zst; do
+            cp "$file" /home/toffski/llvm-git/"$file"
+        done
 
-        cp /home/toffski/toff_build_repo/llvm-ocaml-git-*.pkg.tar.zst \
-            /home/toffski/llvm-git/llvm-ocaml-git.pkg.tar.zst
+        cd /home/toffski/llvm-git || exit
 
-        repo-add llvm-git.db.tar.gz ./*    
-        
+        repo-add llvm-git.db.tar.gz ./*
+
         pacman -Syyu --noconfirm
         ccm n
+
         sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/toffski\/makepkg-x86_64_O3_LLVM\.conf\"/g' \
             /home/toffski/.config/clean-chroot-manager.conf
-        # sed -i -E 's/.*CUSTOM_PACMAN_CONF.*/#CUSTOM_PACMAN_CONF=\"\/etc\/pacman\.conf"/g' \
-        #     /home/toffski/.config/clean-chroot-manager.conf
+
         sed -i -E 's/.*USELLVM.*/#USELLVM=1/g' \
             /home/toffski/.config/clean-chroot-manager.conf
+
         ccm c
         cd /home/toffski/arch-llvm/mesa-git-aur/mesa-git || exit
         sed -i 's/ MESA_WHICH_LLVM=4/ MESA_WHICH_LLVM=2/g' PKGBUILD
-        # sed -i 's/gallium-opencl=icd/gallium-opencl=disabled/g' PKGBUILD
         ccm s
     }
 
-    # build_lib32-llvm-git() {
-    #     pacman -Syyu --noconfirm
-    #     ccm n
-    #     sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/usr\/share\/devtools\/makepkg-x86_64\.conf"/g' \
-    #         /home/toffski/.config/clean-chroot-manager.conf
-    #     sed -i -E 's/.*CUSTOM_PACMAN_CONF.*/CUSTOM_PACMAN_CONF=\"\/usr\/share\/devtools\/pacman-multilib\.conf"/g' \
-    #         /home/toffski/.config/clean-chroot-manager.conf
-    #     ccm c
-    #     cd /home/toffski/arch-llvm/mesa-git-aur/lib32-llvm-git || exit
-    #     ccm s
-    # }
-
-    # build_lib32-mesa-git() {
-    #     pacman -Syyu --noconfirm
-    #     ccm n
-    #     sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/toffski\/makepkg-x86_64_O3\.conf\"/g' \
-    #         /home/toffski/.config/clean-chroot-manager.conf
-    #     sed -i -E 's/.*CUSTOM_PACMAN_CONF.*/CUSTOM_PACMAN_CONF=\"\/usr\/share\/devtools\/pacman-multilib\.conf"/g' \
-    #         /home/toffski/.config/clean-chroot-manager.conf
-    #     ccm c
-    #     cd /home/toffski/arch-llvm/mesa-git-aur/lib32-mesa-git || exit
-    #     sed -i 's/ MESA_WHICH_LLVM=4/ MESA_WHICH_LLVM=2/g' PKGBUILD
-    #     ccm s
-    # }
-
     build_xf86-video-amdgpu-git() {
         pacman -Syyu --noconfirm
-        # sed -i -E 's/.*CUSTOM_PACMAN_CONF.*/#CUSTOM_PACMAN_CONF=\"\/etc\/pacman\.conf"/g' \
-        #     /home/toffski/.config/clean-chroot-manager.conf
         ccm n
         sed -i -E 's/.*USELLVM.*/USELLVM=1/g' \
             /home/toffski/.config/clean-chroot-manager.conf
@@ -93,36 +64,10 @@ elevated_build_tasks() {
         ccm s
     }
 
-    # build_xf86-video-intel-git() {
-    #    pacman -Syyu --noconfirm
-    #    ccm n
-    #    ccm c
-    #    cd /home/toffski/arch-llvm/mesa-git-aur/xf86-video-intel-git || exit
-    #    wget 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/xf86-video-intel/trunk/PKGBUILD'
-    #    wget 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/xf86-video-intel/trunk/xf86-video-intel.install'
-    #    ccm s
-    # }
-    # rebuild_llvm-git() {
-    #     pacman -Syyu --noconfirm
-    #     rm -rf /home/toffski/toff_build_repo/{llvm-git*.pkg.tar.zst, llvm-ocaml-git*.pkg.tar.zst, llvm-libs-git*.pkg.tar.zst}
-    #     sed -i -E 's/.*CUSTOM_PACMAN_CONF.*/#CUSTOM_PACMAN_CONF=\"\/etc\/pacman\.conf"/g' \
-    #         /home/toffski/.config/clean-chroot-manager.conf
-    #     sed -i -E 's/.*CUSTOM_MAKEPKG_CONF.*/CUSTOM_MAKEPKG_CONF=\"\/home\/toffski\/makepkg-x86_64_O3\.conf\"/g' \
-    #         /home/toffski/.config/clean-chroot-manager.conf
-    #     ccm n
-    #     ccm c
-    #     cd /home/toffski/arch-llvm/mesa-git-aur/llvm-git-O3 || exit
-    #     ccm s
-    # }
-
     build_python-sphinx-automodapi || exit
     build_llvm-git || exit
     build_mesa-git || exit
-    # build_lib32-llvm-git || exit
-    # build_lib32-mesa-git || exit
     build_xf86-video-amdgpu-git || exit
-    # build_xf86-video-intel-git || exit
-    # rebuild_llvm-git || exit
 
 }
 
